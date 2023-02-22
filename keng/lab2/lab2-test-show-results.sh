5e369df193453dc1ee1c703ae11399547f85ca58
salsous <salsous@users.noreply.github.com>
2023-02-22 14:59:57 -0500

    Create lab2-test-show-results.sh

---
+++ keng/lab2/lab2-test-show-results.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -xe
+LabDir="/home/ubuntu/keng-td/keng"
+LabId="lab2"
+export OTG_API="https://localhost:8443"
+#
+
+watch -n 1 "curl -sk \"${OTG_API}/results/metrics\" -X POST -H 'Content-Type: application/json' -d '{ \"choice\": \"flow\" }'"
