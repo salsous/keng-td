e273a36029d817f2dcd0da431a771cd439adf404
salsous <salsous@users.noreply.github.com>
2023-02-22 14:37:08 -0500

    Create lab2-test-start-traffic.sh

---
+++ keng/lab2/lab2-test-start-traffic.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -xe
+LabDir="/home/ubuntu/keng-td/keng"
+LabId="lab2"
+export OTG_API="https://localhost:8443"
+#
+
+curl -k "${OTG_API}/control/transmit" -H "Content-Type: application/json" -d '{"state" : "start"}'
