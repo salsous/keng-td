86d1b076090afd320484f722346c10b5d9dbe6e5
salsous <salsous@users.noreply.github.com>
2023-02-22 14:33:04 -0500

    Update and rename lab2-test-unidirectional.sh to lab2-test-apply-config.sh

---
+++ keng/lab2/lab2-test-apply-config.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -xe
+LabDir="/home/ubuntu/keng-td/keng"
+LabId="lab2"
+export OTG_API="https://localhost:8443"
+#
+curl -k "${OTG_API}/config" -H "Content-Type: application/json" -d @$LabDir/$LabId/otg-config.json
--- keng/lab2/lab2-test-unidirectional.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -xe
-export OTG_API="https://localhost:8443"
-#
-python lab2-test-unidirectional.py
